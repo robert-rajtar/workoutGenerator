d807d8d721a4d3219d006853da82244e954810c4
Robert <rr.rajtar@gmail.com>
2024-03-15 23:08:45 +0100

    refactor function select exercise

--- generator.py
+++ generator.py
@@ -41,76 +41,65 @@ def get_user_name():
 def select_exercises(db, groups):
     selected_exercises = {}
 
-    # Iterate over each exercise group
     for group in groups:
-        print(f"\n{group}:")
         exercises = db.get_exercises_by_group(group)
-
-        # Check if there are any exercises in the group
         if not exercises:
             print("No exercises found in this group.")
             continue
 
-        # Display the list of exercises in the group
-        for exercise in exercises:
-            print(f"{exercise[0]}. {exercise[1]}")
-
-        # Determine the range of exercise numbers for the group
-        start_number = exercises[0][0]
-        end_number = exercises[-1][0]
-
-        # Prompt the user to choose two exercises from the group
-        while True:
-            selected_exercise_ids = input(
-                f"Choose two exercises from the {group} group (enter numbers, separated by space): ").split()
-            valid_selection = True
-
-            # Validate if the exercise numbers are within the available range
-            for exercise_id in selected_exercise_ids:
-                try:
-                    exercise_id = int(exercise_id)
-                    if not start_number <= exercise_id <= end_number:
-                        print(
-                            f"Invalid exercise number: {exercise_id}. Please choose within the available range.")
-                        valid_selection = False
-                        break
-                except ValueError:
-                    print("Invalid input. Please enter numbers only.")
-                    valid_selection = False
-                    break
-
-            # Check if exactly two exercises were selected
-            if valid_selection and len(selected_exercise_ids) == 2:
-                break
-
-        # Prompt the user to input the number of series and repetitions for each exercise
-        for exercise_id in selected_exercise_ids:
-            try:
-                exercise_id = int(exercise_id)
-                selected_exercise = [exercise for exercise in exercises if exercise[0] == exercise_id][0]
-
-                # Prompt the user for the number of series
-                while True:
-                    try:
-                        series = int(input(f"How many series of exercise '{selected_exercise[1]}'? "))
-                        break
-                    except ValueError:
-                        print("Invalid input. Please enter numbers only.")
-
-                # Prompt the user for the number of repetitions
-                while True:
-                    try:
-                        repetitions = int(
-                            input(f"How many repetitions in each series of exercise '{selected_exercise[1]}'? "))
-                        break
-                    except ValueError:
-                        print("Invalid input. Please enter numbers only.")
-
-                selected_exercises[selected_exercise[1]] = {'series': series, 'repetitions': repetitions}
-            except ValueError:
-                print("Invalid input. Please enter numbers only.")
+        print_exercises(exercises)
+        selected_exercise_ids = get_user_selected_exercises(exercises)
+        selected_exercises.update(get_exercise_details(exercises, selected_exercise_ids))
 
     return selected_exercises
+
+
+def print_exercises(exercises):
+    print(f"\n{exercises[0][1]}:")
+    for exercise in exercises:
+        print(f"{exercise[0]}. {exercise[1]}")
+
+
+def get_user_selected_exercises(exercises):
+    start_number = exercises[0][0]
+    end_number = exercises[-1][0]
+    while True:
+        selected_exercise_ids = input(f"Choose two exercises from the group (enter numbers, " 
+                                      "separated by space): ").split()
+        if validate_exercise_ids(selected_exercise_ids, start_number, end_number):
+            return selected_exercise_ids
+
+
+def validate_exercise_ids(selected_exercise_ids, start_number, end_number):
+    if len(selected_exercise_ids) != 2:
+        print("Please select exactly two exercises.")
+        return False
+    for exercise_id in selected_exercise_ids:
+        try:
+            exercise_id = int(exercise_id)
+            if not start_number <= exercise_id <= end_number:
+                print(f"Invalid exercise number: {exercise_id}. Please choose within the available range.")
+                return False
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+            return False
+    return True
+
+
+def get_exercise_details(exercises, selected_exercise_ids):
+    exercise_details = {}
+    for exercise_id in selected_exercise_ids:
+        exercise_id = int(exercise_id)
+        selected_exercise = next(exercise for exercise in exercises if exercise[0] == exercise_id)
+        series, repetitions = get_series_and_repetitions(selected_exercise[1])
+        exercise_details[selected_exercise[1]] = {'series': series, 'repetitions': repetitions}
+    return exercise_details
+
+
+def get_series_and_repetitions(exercise_name):
+    series = int(input(f"How many series of exercise '{exercise_name}'? "))
+    repetitions = int(input(f"How many repetitions in each series of exercise '{exercise_name}'? "))
+    return series, repetitions
 
 
 def generate_pdf(name, selected_exercises, pdf_report):
